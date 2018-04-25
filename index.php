00dc172d70efdafe2032d4818f7dde089bf60878
Lucas Johns <johnslw26@mounties.mansfield.edu>
2018-04-25 11:06:45 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Browndale Fire Dashboard</title>
+        <meta charset="UTF-8" name="description" content = "Administrative dashboard for reports, inventory, and member information" />
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+    </head>
+    <body>
+        <header class="top-bar">
+            <div class="top-bar-container">
+                <div class="top-bar-main">
+                    <div class="logo"><p>Browndale Fire Department Management System</p></div>
+                </div>
+                <div class="top-bar-sub">
+                    
+                </div>
+            </div>
+        </header>
+        <div class="sidebar">
+            <div class="sidebar-container">
+                <div class="sidebar-main">
+                    <div class="account-bar">
+                        <a class="logolink" href="index.php">DMS</a>
+                    </div>
+                    <ul class="sidebar-list">
+                        <li class="sidebar-link"><a class="sidebar-dashboard" href="index.php"><img class="dashlogo" src="images/dash.png" />Dashboard</a></li>
+                        <li class="sidebar-link"><a class="sidebar-training" href="index.php"><img class="dashlogo" src="images/training.png" />Training</a></li>
+                        <li class="sidebar-link"><a class="sidebar-inventory" href="index.php"><img class="dashlogo" src="images/inventory.png" />Inventory</a></li>
+                        <li class="sidebar-link"><a class="sidebar-reports" href="index.php"><img class="dashlogo" src="images/reports.png" />Reports</a></li>
+                        <li class="sidebar-link"><a class="sidebar-pictures" href="index.php"><img class="dashlogo" src="images/pictures.png" />Pictures</a></li>
+                        <li class="sidebar-link"><a class="sidebar-contact" href="index.php"><img class="dashlogo" src="images/contact.png" />Members</a></li>    
+                        <li class="divider"></li>
+                        <li class="sidebar-link"><a class="sidebar-login" href="index.php"><img class="dashlogo" src="images/arrowright.png" />Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
